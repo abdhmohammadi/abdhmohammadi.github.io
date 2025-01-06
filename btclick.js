a2abec96ea0b81d5ecb93a540336e74862625bba
Abdh <AbdhMohammady@gmail.com>
2025-01-07 00:55:48 +0330

    Delete btclick.js

--- btclick.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('myButton').addEventListener('click', 
-        function() 
-        {
-          alert('This page is under construction ...!');
-        /* https://abdhmohammadi.github.io/sources/teaching/mathematics-education-root.html*/
-        }
-);
