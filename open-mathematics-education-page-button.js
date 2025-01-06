f8af10fb0bee5a9063842a65355b68d5e059a28f
Abdh <AbdhMohammady@gmail.com>
2025-01-07 00:54:43 +0330

    Update open-mathematics-education-page-button.js

--- open-mathematics-education-page-button.js
+++ open-mathematics-education-page-button.js
@@ -1,1 +1,7 @@
-
+document.getElementById('open-mathematics-education-page-button').addEventListener('click', 
+        function() 
+        {
+          alert('This page is under construction ...!');
+        /* https://abdhmohammadi.github.io/sources/teaching/mathematics-education-root.html*/
+        }
+);
