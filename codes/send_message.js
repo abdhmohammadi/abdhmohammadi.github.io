e2eba29f947c5b61b6fea15109497d00c7fe687b
Abdh <AbdhMohammady@gmail.com>
2025-01-14 00:13:06 +0330

    Update send_message.js

--- codes/send_message.js
+++ codes/send_message.js
@@ -1,10 +1,15 @@
 function sendMessage()
 {
-  const message = document.getElementById('message').value;
-  const botToken = 'AAFk5DsbuPPKRX35-EwMSRkIYBaIeq5oSs';
-  const chatId = '101548936';
-  fetch(`https://api.telegram.org/bot${botToken}/sendMessage?chat_id=${chatId}&text=${encodeURIComponent(message)}`)
-    .then(response => response.json())
-    .then(data => console.log(data))
-    .catch(error => console.error('Error:', error)); 
+    const message = document.getElementById('message').value;
+    const botToken = 'AAFk5DsbuPPKRX35-EwMSRkIYBaIeq5oSs';
+    const chatId = '101548936';
+
+    fetch(`https://api.telegram.org/bot${botToken}/sendMessage?chat_id=${chatId}&text=${encodeURIComponent(message)}`)
+        .then(response => response.json())
+        .then(data => console.log(data))
+        .catch(error =>
+          {
+            console.error('Error:', error));
+            alert('An error occurred while sending the message: ' + error.message);
+          }
 }
