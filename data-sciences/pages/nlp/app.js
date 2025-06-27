d68e05474f41bed9dbb663ccec61ee4638561aaa
abdhmohammadi <abdhmohammady@gmail.com>
2025-06-27 22:54:05 +0330

    update

--- data-sciences/pages/nlp/app.js
+++ data-sciences/pages/nlp/app.js
@@ -3,6 +3,9 @@ async function loadDictionary()
   const response = await fetch('nlp-sp-dic.json');
   const data = await response.json();
   displayEntries(data);
+  
+  vocab = document.getElementById('vocabulary-count');
+  vocab.textContent = `${data.length} words`;
 
   document.getElementById('searchBox').addEventListener('input', () => {
     const query = document.getElementById('searchBox').value.toLowerCase();
